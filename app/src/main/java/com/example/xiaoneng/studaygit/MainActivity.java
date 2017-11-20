f6bd96556603b28725853be4fe76e974ee2d5fcd
15210183057 <15210183057@163.com>
2017-11-20 12:07:12 +0800

    更新测试

---
+++ app/src/main/java/com/example/xiaoneng/studaygit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.xiaoneng.studaygit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //创建
+        setContentView(R.layout.activity_main);
+    }
+}
